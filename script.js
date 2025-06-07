fead7fa02bc9d9895eecfa02da2e723eea6784dd
k4i-j <kaijief0@gmail.com>
2025-06-08 02:23:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// Typing animation
-const text = "Aspiring Mechatronics Engineer | Coder | Innovator";
-let i = 0;
-const speed = 50;
-const introText = document.getElementById("introText");
-
 // Toggle dark mode
 document.getElementById('darkToggle').addEventListener('click', () => {
   document.body.classList.toggle('dark');
 });
+
+// Typing animation
+const text = "Aspiring Mechatronics Engineer And Innovator";
+let i = 0;
+const speed = 50;
+const introText = document.getElementById("introText");
 
 // Animate on scroll
 const fadeElements = document.querySelectorAll('.fade-in');
